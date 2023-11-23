35a4464ee26b7162754bca242da00fb0f40f9ce8
Csaba Toth <csabi627@gmail.com>
2023-11-23 16:28:43 +0100

    feat:initial dotenv

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const userRouter = require('./routes/userRoutes');
 const tourRouter = require('./routes/tourRoutes')
 
 const app = express();
+
+if(process.env.NODE_ENV === "development"){
+  app.use(morgan('dev'));
+
+}
+
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`))
 
-app.use(morgan('dev'));
+
 
 app.use((req, res, next) => {
   console.log('Hello from the middleware');
