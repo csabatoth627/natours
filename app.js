56857bead16ff81d4c1668e81de19df6b4ad2334
Csaba Toth <csabi627@gmail.com>
2023-11-23 14:53:03 +0100

    feat:create ID check middleware(tourController.js)

--- app.js
+++ app.js
@@ -19,8 +19,5 @@ app.use('/api/v1/tours', tourRouter);
 
 
 
-const port = 3000;
 
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,36 +1,44 @@
+exports.checkId = (req, res, next, val) => {
+  if (val > 1) {
+    return res.status(404).json({
+      status: 'fail',
+      massage: 'invalid ID',
+    });
+  }
+  next();
+};
 
 exports.getAllTours = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
-    });
-  };
-  
-  exports.getTour = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
-    });
-  };
-  
-  exports.createTour = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
-    });
-  };
-  
-  exports.updateTour = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
-    });
-  };
-  
-  exports.deleteTour = (req, res) => {
-    res.status(500).json({
-      status: 'error',
-      message: 'This route is not yet defined!',
-    });
-  };
-  
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.getTour = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.createTour = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.updateTour = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
+
+exports.deleteTour = (req, res) => {
+  res.status(500).json({
+    status: 'error',
+    message: 'This route is not yet defined!',
+  });
+};
